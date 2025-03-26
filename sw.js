7155a369b79d85acc0f6d39dee71c20f2dd5f2dd
Hido81 <marco@idolazzi.it>
2025-03-26 23:57:56 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,21 @@
-const CACHE_NAME = 'qr-scanner-v1';
+const CACHE_NAME = 'qr-scanner-v2';
 const ASSETS = [
   '/',
   '/index.html',
-  '/styles/main.css',
+  '/manifest.json',
   'https://cdnjs.cloudflare.com/ajax/libs/html5-qrcode/2.3.4/html5-qrcode.min.js'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
+      .then((cache) => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then((response) => response || fetch(event.request))
   );
 });
